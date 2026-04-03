4a541961129b99bc991684dd28856c0beb603aa5
Sapna24Sangmitra <sapna.sangmitra@sjsu.edu>
2026-04-03 10:26:39 -0700

    Leetcode: 9, 134, 269, 410, 767

---
+++ Problems/767.reorganize_string.py
@@ -0,0 +1,29 @@
+from lc import *
+
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s) 
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
+        heapq.heapify(maxHeap)
+
+        prev = None
+        res = ""
+        while maxHeap or prev:
+            if prev and not maxHeap:
+                return ""
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            if cnt!= 0:
+                prev = [cnt, char]
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.reorganizeString("aab"))
---
+++ Problems/9.palindrome_number.py
@@ -0,0 +1,29 @@
+from lc import *
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+
+        while x:
+            right = x % 10
+            left = x // div
+
+            if left != right: 
+                return False
+            
+            x = (x % div) // 10
+            div = div / 100
+
+        return True
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isPalindrome(121))
+    print(s.isPalindrome(-121))
+    print(s.isPalindrome(10))
